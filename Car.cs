8662c4ebf583943aa363ecbaf9597c629f99fc82
Washington Yandun <washington.yandun.morales@gmail.com>
2023-06-22 01:19:05 -0600

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BridgePatternConsoleExample
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        [StringLength(150)]
+        public string Name { get; set; }
+        [StringLength(150)]
+        public string Model { get; set; }
+        [StringLength(150)]
+        public string Color { get; set; }
+        [StringLength(150)]
+        public string Brand { get; set; }
+        [Range(1950, int.MaxValue)]
+        public int Year { get; set; }
+    }
+
+    public interface ICar
+    {
+        void DisplayDetails();
+    }
+
+    public class BasicCar : ICar
+    {
+        private readonly Car _car;
+
+        public BasicCar(Car car)
+        {
+            _car = car;
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Basic Car Details: {_car.Brand} {_car.Model}, {_car.Year}");
+        }
+    }
+
+    public class DetailedCar : ICar
+    {
+        private readonly Car _car;
+
+        public DetailedCar(Car car)
+        {
+            _car = car;
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Detailed Car Details: {_car.Brand} {_car.Model}, {_car.Year}, {_car.Color}");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using BridgePatternConsoleExample;
+
+var car = new Car
+{
+    Id = 1,
+    Name = "My Car",
+    Brand = "Toyota",
+    Model = "Camry",
+    Color = "Blue",
+    Year = 2020
+};
+
+// Utilizar la implementación BasicCar para mostrar los detalles básicos del automóvil
+ICar basicCar = new BasicCar(car);
+basicCar.DisplayDetails();
+
+// Utilizar la implementación DetailedCar para mostrar los detalles detallados del automóvil
+ICar detailedCar = new DetailedCar(car);
+detailedCar.DisplayDetails();
+
+Console.ReadLine();
